3a1f646035783577d84df6d6339fb821cc184b7c
Over Heaven <greenreedseedwood@gmail.com>
2023-11-28 12:17:46 -0600

    Added Start Screen

--- sketch.js
+++ sketch.js
@@ -1,19 +1,15 @@
-///////////////////////////////////////////////////////////////////////////////////////////
-// Warehouse Weed
+///////////////////////////////////////////////////////////////////////////////////////////////
+// Grenehoreh
 // Andrew Chen
 // 11/21/2023
 //
-// Extra for Experts:
-// - loaded fonts
-// - 3D Arrays
-///////////////////////////////////////////////////////////////////////////////////////////
-// Game: You are a gardener who grows and sells weeds.
+///////////////////////////////////////////////////////////////////////////////////////////////
+// Game: An Upstart Gardener takes over an abandoned greenhouse to begin their produce empire!
 //
-// Instructions: WASD to move character. (it doesn't do anything at the moment)
-// Moving out of the warehouse turns the player into truck mode, which elongates the character.
-// Mouse Press to plant and harvest weeds. Space press to mass harvest weeds. Up and Down 
-// Arrows filter through the different weeds.
-///////////////////////////////////////////////////////////////////////////////////////////
+// Instructions: WASD Movement. Mouse Click to plant seeds and harvest plants. Spacebar harvests
+// all plants. Plants cost money, but make back money when they are harvested. Arrow Keys Cycle
+// through different plants.
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 
 // Variables
@@ -26,8 +22,8 @@ let maps = {
 };
 
 let state = {
-  size: [1.5, 1.5],
   plant: 0,
+  growth: [[[136, 82, 127], [159, 135, 175], [188, 231, 253], [169, 237, 190], [80, 132, 132]], [[54, 60, 60], [65, 193, 241], [52, 110, 129], [152, 251, 152], [27, 131, 102]], [[54, 60, 60], [71, 125, 139], [60, 155, 162], [105, 162, 151], [48, 105, 100]]]
 };
 
 let cellSize;
@@ -37,23 +33,25 @@ let font;
 let market = [[100, 250], [500, 1000], [2000, 5000]];
 let player;
 let playerImage;
+let startScreen;
+let begin = false;
 
 // Loads Font
 function preload() {
   font = loadFont("Pixel Font.TTF");
+  startScreen = loadImage("Start Screen.png");
 }
 
 // Builds Map, Sets up Text Scale
 function setup() {
-  imageMode(CENTER);
   createCanvas(windowWidth, windowHeight);
-  background(0);
   resizeScale();
   textFont(font);
   cellSize = min(windowHeight, windowWidth) / GRID_SIZE;
   maps.lobbyMap = genMap();
   maps.data = genData();
   player = new Player(GRID_SIZE / 2, GRID_SIZE / 2, 100);
+  image(startScreen, 0, 0, windowWidth, windowHeight);
 }
 
 // Centers the grid
@@ -63,20 +61,22 @@ function resizeScale() {
 }
 
 function mousePressed() {
+  begin = true;
+  background(0);
   // Variables corresponding to mouse pos within grid
-  let px = floor((mouseX - xOffset) / cellSize);
-  let py = floor((mouseY - yOffset) / cellSize);
+  let x = floor((mouseX - xOffset) / cellSize);
+  let y = floor((mouseY - yOffset) / cellSize);
 
   // Plants and collects weeds
-  if (maps.data[px][py].growth === 4) {
-    player.wallet += market[maps.data[px][py].state - 2][1];
-    maps.data[px][py] = 1; 
-    maps.lobbyMap[px][py] = color(random(220, 230));
+  if (maps.data[x][y].growth === 4) {
+    player.wallet += market[maps.data[x][y].state - 2][1];
+    maps.data[x][y] = 1; 
+    maps.lobbyMap[x][y] = color(random(220, 230));
   }
   else {
-    if (maps.data[px][py] === 1 && py < 34 && player.wallet >= market[state.plant][0]) {
-      maps.data[px][py] = new Plant(state.plant + 2, 0);
-      maps.lobbyMap[px][py] = color(state.growth[state.plant][0]);
+    if (maps.data[x][y] === 1 && y < 34 && player.wallet >= market[state.plant][0]) {
+      maps.data[x][y] = new Plant(state.plant + 2, 0);
+      maps.lobbyMap[x][y] = color(state.growth[state.plant][0]);
       player.wallet -= market[state.plant][0];
     }
   }
@@ -84,36 +84,41 @@ function mousePressed() {
 
 function keyPressed() {
   // Collects all Plants
-  if (keyCode === 32) {
-    for (let x = 0; x < GRID_SIZE; x++) {
-      for (let y = 0; y < 34; y++) {
-        if (maps.data[x][y].growth === 4) {
-          player.wallet += market[maps.data[x][y].state - 2][1];
-          maps.data[x][y] = 1;
-          maps.lobbyMap[x][y] = color(random(220, 230));
+  if (begin === true) {
+    if (keyCode === 32) {
+      for (let x = 0; x < GRID_SIZE; x++) {
+        for (let y = 0; y < 34; y++) {
+          if (maps.data[x][y].growth === 4) {
+            player.wallet += market[maps.data[x][y].state - 2][1];
+            maps.data[x][y] = 1;
+            maps.lobbyMap[x][y] = color(random(220, 230));
+          }
         }
       }
     }
-  }
 
-  // Changes current plant
-  if (keyCode === 38) {
-    state.plant++;
+    // Changes current plant
+    if (keyCode === 38) {
+      state.plant++;
+    }
+    else if (keyCode === 40) {
+      state.plant++;
+    }
+    state.plant = state.plant % 3;
   }
-  else if (keyCode === 40) {
-    state.plant++;
-  }
-  state.plant = state.plant % 3;
 }
 
 // Simulates the game
 function draw() {
-  noStroke();
-  drawMap(GRID_SIZE, GRID_SIZE);
-  drawPlants();
-  drawUI();
-  player.update();
-  player.display();
+  if (begin === true) {
+    noStroke();
+    drawMap(GRID_SIZE, GRID_SIZE);
+    drawPlants();
+    drawUI();
+    player.update();
+    player.display();
+  }
+  console.log(begin);
 }
 
 // Loads the map
@@ -257,26 +262,26 @@ function drawUI() {
   noStroke();
   textSize(3 * cellSize);
   fill(22, 129, 44);
-  text(player.wallet, 1.75 * cellSize + 2, 4 * cellSize + 2);
+  text(player.wallet, 2 * cellSize, 4.25 * cellSize);
   fill(87, 196, 120);
   text(player.wallet, 1.75 * cellSize, 4 * cellSize);
 
   // Plant Text
   textSize(3.75 * cellSize);
   fill(textStroke);
-  text(char, 1.3 * cellSize + 2, 10.5 * cellSize + 2);
+  text(char, 1.5 * cellSize, 10.7 * cellSize);
   fill(textFill);
   text(char, 1.3 * cellSize, 10.5 * cellSize);
 
   textSize(2.75 * cellSize);
   fill(textStroke);
-  text(name, 6.5 * cellSize + 2, 10 * cellSize + 2);
+  text(name, 6.7 * cellSize, 10.2 * cellSize);
   fill(textFill);
   text(name, 6.5 * cellSize, 10 * cellSize);
 
   textSize(2 * cellSize);
   fill(textStroke);
-  text(desc, 1.5 * cellSize + 2, 14.25 * cellSize + 2, xOffset - cellSize);
+  text(desc, 1.7 * cellSize, 14.45 * cellSize, xOffset - cellSize);
   fill(textFill);
   text(desc, 1.5 * cellSize, 14.25 * cellSize, xOffset - cellSize);
 }
@@ -286,7 +291,6 @@ class Plant {
   constructor(state, growth) {
     this.state = state;
     this.growth = growth;
-    this.stages = [[136, 82, 127], [159, 135, 175], [188, 231, 253], [169, 237, 190], [80, 132, 132]], [[54, 60, 60], [65, 193, 241], [52, 110, 129], [152, 251, 152], [27, 131, 102]], [[54, 60, 60], [71, 125, 139], [60, 155, 162], [105, 162, 151], [48, 105, 100]];
   }
 
   update() {
